e49d253c3404eac724b46b5a423b4a22cfba0f91
home <kioho0250@gmail.com>
2018-03-24 23:15:19 +0900

    first commit

---
+++ src/main/java/kr/co/firstproject/core/WebAppInitializer.java
@@ -0,0 +1,34 @@
+package kr.co.firstproject.core;
+
+import kr.co.firstproject.core.config.AppConfig;
+import kr.co.firstproject.core.config.WebServletConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebServletConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+
+        return new Filter[]{encodingFilter};
+    }
+
+
+}
---
+++ src/main/java/kr/co/firstproject/core/config/AppConfig.java
@@ -0,0 +1,7 @@
+package kr.co.firstproject.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
